2ca1413494ae050ee5b0048aa3a6336b732fe027
Prashant Ladha <prashant.ladha@gmail.com>
2018-11-16 21:20:57 -0500

    newly updated code

--- HelloWorldApplication/src/com/ruchitaladha/Application.java
+++ HelloWorldApplication/src/com/ruchitaladha/Application.java
@@ -5,6 +5,7 @@ public class Application {
 	public static void main(String[] args) {
 		System.out.println("Hello World Application!!!");
 		System.out.println("Updated code!!");
+		System.out.println("Newly updated code!!");
 	}
 
 }
